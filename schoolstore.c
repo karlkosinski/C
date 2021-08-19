3f5d04b5132da9ed9e32b45f7a1628df788af19d
karlkosinski <79778772+karlkosinski@users.noreply.github.com>
2021-08-19 16:36:51 -0400

    Update schoolstore.c

--- schoolstore.c
+++ schoolstore.c
@@ -3,7 +3,7 @@
 
 int main() {
 
-	int pencils = 0;                                     // Allocate and initilize memory
+	int pencils = 0;                                     				   // Allocate and initilize memory
 	double price = 0, subtotal = 0, tax = 0, total = 0;
 	
 	printf("\n                Name:               Karl                             "); // initial student name
@@ -16,11 +16,11 @@ int main() {
 	printf("\n");
 	printf("\n");
 
-	printf("\n                Enter the number of pencils to purchased: "); // # of pencils
+	printf("\n                Enter the number of pencils to purchased: "); 	   // # of pencils
 	scanf_s("%d", &pencils);
 	printf("\n");
 
-	printf("\n                Enter the price of one pencil: $ "); // price of one pencil 
+	printf("\n                Enter the price of one pencil: $ ");                    // price of one pencil 
 	scanf_s("%lf", &price);
 	printf("\n");
 
@@ -34,13 +34,13 @@ int main() {
 	printf("\n                  Price of each pencil: $ %lf", price);
 	printf("\n");
 
-	printf("\n                  Sub Total: $ %lf", subtotal); // Subtotal 
+	printf("\n                  Sub Total: $ %lf", subtotal); 			  // Subtotal 
 	printf("\n");
 
-	printf("\n                  Tax Amount at 13%% HST: $ %lf", tax); // Hst 13%
+	printf("\n                  Tax Amount at 13%% HST: $ %lf", tax);                 // Hst 13%
 	printf("\n");
 
-	printf("\n                  Total Amount Due Now: $ %lf", total); // total = subtotal + tax 
+	printf("\n                  Total Amount Due Now: $ %lf", total);                 // total = subtotal + tax 
 	printf("\n");
 
 
